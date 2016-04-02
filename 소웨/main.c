e224ed0d72f7d49d8e7a0339d5ca6af8e6319d48
심지훈 <shimjihoon@simjihun-ui-macbook-pro.local>
2016-04-03 08:01:22 +0900

    Initial Commit

---
+++ 소웨/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  소웨
+//
+//  Created by 심지훈 on 2016. 4. 3..
+//  Copyright © 2016년 심지훈. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
